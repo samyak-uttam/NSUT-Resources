b07b3c36e79399baf89c8bff04c70db8aae37748
Your Name <samyakuttam@gmail.com>
2019-10-01 11:34:54 +0530

    small changes

--- app/src/main/java/com/example/nsutallin1/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/nsutallin1/Fragments/HomeFragment.java
@@ -6,20 +6,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.nsutallin1.CollegeActivity;
 import com.example.nsutallin1.NoticesActivity;
 import com.example.nsutallin1.R;
 
 public class HomeFragment extends Fragment {
 
+    LinearLayout clgStuff;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        clgStuff = (LinearLayout) rootView.findViewById(R.id.clg_stuff);
+        clgStuff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CollegeActivity.class);
+                startActivity(intent);
+            }
+        });
 
         ImageView noticesImage = (ImageView) rootView.findViewById(R.id.notices_and_circulars);
         noticesImage.setOnClickListener(new View.OnClickListener() {
